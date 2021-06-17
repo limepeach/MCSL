0cb67607442a13705f8b4b2038fe616edc86bb26
Bluemangoo <chenfy2006@qq.com>
2021-06-17 20:53:45 +0800

    2106172053

---
+++ src/com/mcsl/Server.java
@@ -0,0 +1,20 @@
+package com.mcsl;
+
+import java.util.*;
+import java.awt.*;
+import java.io.File;
+import javax.swing.*;
+
+public class Server {
+	String serverName;
+	File fileSettings, fileOptions;
+	Properties serverSettings,serverOptions;
+	
+
+	public Server(String theName) {
+		this.serverName = theName;
+
+	}
+	
+	
+}
