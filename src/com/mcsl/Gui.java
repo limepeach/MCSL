4e6c29906b7ec6c03fa1d9337534e65ef95b63d7
Bluemangoo <chenfy2006@qq.com>
2021-06-21 14:08:16 +0800

    2021年6月21日14点08分

--- src/com/mcsl/Gui.java
+++ src/com/mcsl/Gui.java
@@ -22,14 +22,27 @@ public class Gui extends Application {
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
+
 	String pageNow;
-	public void refresh() {
-		
+	public class StartPage extends StackPane{
+		public StartPage() {
+			super();
+			Label startPageInfo=new Label("欢迎使用MCSL，目前版本为0.0.0.1");
+			StackPane.setAlignment(startPageInfo, Pos.CENTER_RIGHT);
+			
+		}
 	}
-	public void setButtonType(Button menuButton,boolean type) {
+	public void refresh() {
+		if(pageNow=="Start") {
+			
+		}
+	}
+
+	public void setButtonType(Button menuButton, boolean type) {
 		menuButton.setDisable(!type);
 		menuButton.setStyle("-fx-opacity: 1;");
 	}
+
 	@Override
 	public void start(Stage primaryStage) {
 
@@ -59,8 +72,9 @@ public class Gui extends Application {
 		primaryStage.setWidth(810);
 		primaryStage.setHeight(490);
 		BorderPane pane = new BorderPane();
+		pane.setId("biggest_pane");
 		pane.setPadding(new Insets(0, 0, 0, 0));// 下左上右
-		pane.setStyle("-fx-background-color: #FFFFF1");//背景
+		pane.setStyle("-fx-background-color: #FFFFF1；-fx-background-image: url(\"/com/mcsl/resouse/pic/background.png\");");// 背景
 
 		Image start_unselected = new Image("/com/mcsl/resouse/pic/start_unselected.png");
 		Image start_selected = new Image("/com/mcsl/resouse/pic/start_selected.png");
@@ -84,15 +98,28 @@ public class Gui extends Application {
 //		menuButton_Manage.setStyle(MENU_UNSELECTED);
 //		menuButton_Run.setStyle(MENU_UNSELECTED);
 //		menuButton_Settings.setStyle(MENU_UNSELECTED);
+
+		// 加载默认样式
 		menuButton_Start.setId("menu");
 		menuButton_Manage.setId("menu");
 		menuButton_Run.setId("menu");
 		menuButton_Settings.setId("menu");
-		pageNow="Start";
+
+		// 设置default page
+		pageNow = "Start";
+		setButtonType(menuButton_Start, false);
+		menuButton_Start.setGraphic(new ImageView(start_selected));
+		setButtonType(menuButton_Manage, true);
+		menuButton_Manage.setGraphic(new ImageView(manage_unselected));
+		setButtonType(menuButton_Run, true);
+		menuButton_Run.setGraphic(new ImageView(run_unselected));
+		setButtonType(menuButton_Settings, true);
+		menuButton_Settings.setGraphic(new ImageView(settings_unselected));
+
 		menuButton_Start.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Start,false);
+				setButtonType(menuButton_Start, false);
 				menuButton_Start.setGraphic(new ImageView(start_selected));
 				setButtonType(menuButton_Manage, true);
 				menuButton_Manage.setGraphic(new ImageView(manage_unselected));
@@ -100,14 +127,14 @@ public class Gui extends Application {
 				menuButton_Run.setGraphic(new ImageView(run_unselected));
 				setButtonType(menuButton_Settings, true);
 				menuButton_Settings.setGraphic(new ImageView(settings_unselected));
-				pageNow="Start";
+				pageNow = "Start";
 				refresh();
 			}
 		});
 		menuButton_Manage.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Manage,false);
+				setButtonType(menuButton_Manage, false);
 				menuButton_Manage.setGraphic(new ImageView(manage_selected));
 				setButtonType(menuButton_Start, true);
 				menuButton_Start.setGraphic(new ImageView(start_unselected));
@@ -115,14 +142,14 @@ public class Gui extends Application {
 				menuButton_Run.setGraphic(new ImageView(run_unselected));
 				setButtonType(menuButton_Settings, true);
 				menuButton_Settings.setGraphic(new ImageView(settings_unselected));
-				pageNow="Manage";
+				pageNow = "Manage";
 				refresh();
 			}
 		});
 		menuButton_Run.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Run,false);
+				setButtonType(menuButton_Run, false);
 				menuButton_Run.setGraphic(new ImageView(run_selected));
 				setButtonType(menuButton_Start, true);
 				menuButton_Start.setGraphic(new ImageView(start_unselected));
@@ -130,14 +157,14 @@ public class Gui extends Application {
 				menuButton_Manage.setGraphic(new ImageView(manage_unselected));
 				setButtonType(menuButton_Settings, true);
 				menuButton_Settings.setGraphic(new ImageView(settings_unselected));
-				pageNow="Run";
+				pageNow = "Run";
 				refresh();
 			}
 		});
 		menuButton_Settings.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Settings,false);
+				setButtonType(menuButton_Settings, false);
 				menuButton_Settings.setGraphic(new ImageView(settings_selected));
 				setButtonType(menuButton_Start, true);
 				menuButton_Start.setGraphic(new ImageView(start_unselected));
@@ -145,12 +172,12 @@ public class Gui extends Application {
 				menuButton_Manage.setGraphic(new ImageView(manage_unselected));
 				setButtonType(menuButton_Run, true);
 				menuButton_Run.setGraphic(new ImageView(run_unselected));
-				pageNow="Settings";
+				pageNow = "Settings";
 				refresh();
 			}
 		});
 
-		HBox menuBox = new HBox(35);
+		HBox menuBox = new HBox(30);
 		menuBox.getChildren().addAll(menuButton_Start, menuButton_Manage, menuButton_Run, menuButton_Settings);
 		menuBox.setAlignment(Pos.CENTER);
 		menuBox.setPrefHeight(50);
@@ -159,13 +186,15 @@ public class Gui extends Application {
 		menuButton_Manage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Run.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Settings.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+		menuBox.setStyle("-fx-background-color: #FFFFFF");
 		HBox.setHgrow(menuButton_Start, Priority.ALWAYS);
 		HBox.setHgrow(menuButton_Manage, Priority.ALWAYS);
 		HBox.setHgrow(menuButton_Run, Priority.ALWAYS);
 		HBox.setHgrow(menuButton_Settings, Priority.ALWAYS);
+		
 
 		pane.setBottom(menuBox);
-		pane.getStylesheets().add(Gui.class.getResource("style.css").toString());
+		pane.getStylesheets().add(Gui.class.getResource("/com/mcsl/resouse/css/style.css").toString());
 //		root.getChildren().addAll(top,pane);
 
 		Scene scene = new Scene(pane, 200, 250);
