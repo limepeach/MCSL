7d572d10ac577beecc501b50b5ee0af12c0b0456
Bluemangoo <chenfy2006@qq.com>
2021-06-22 09:42:45 +0800

    2021年6月22日09点42分

--- src/com/mcsl/Gui.java
+++ src/com/mcsl/Gui.java
@@ -1,5 +1,7 @@
 package com.mcsl;
 
+import java.security.SecurityPermission;
+
 //import jfxtras.styles.jmetro.JMetro;
 //import jfxtras.styles.jmetro.Style;
 //import jfxtras.styles.jmetro.Style.*;
@@ -14,32 +16,93 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.*;
+import javafx.scene.shape.*;
 import javafx.stage.*;
 import javafx.event.*;
-import javafx.scene.layout.Pane;
 
 public class Gui extends Application {
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
 
+	
+	Pane page=new Pane();
+	BorderPane pane = new BorderPane();
 	String pageNow;
-	public class StartPage extends StackPane{
+	
+	
+	
+	public class StartPage extends AnchorPane{
 		public StartPage() {
 			super();
-			Label startPageInfo=new Label("欢迎使用MCSL，目前版本为0.0.0.1");
-			StackPane.setAlignment(startPageInfo, Pos.CENTER_RIGHT);
+			this.setStyle("-fx-background-color:rgba(255, 255, 255, .0);");
+			Label startPageInfo=new Label(
+					  "	当前版本为0.0.0.1\r\n"
+					+ "	目前MCSL任然处于内测中\r\n"
+					+ "	功能不甚完善\r\n"
+					+ "	有什么建议欢迎添加交流群\r\n"
+					+ "	群号：857934957\r\n"
+					+ "	感谢各位赞助者们！\r\n");
+			startPageInfo.setStyle("-fx-text-fill:#000000;"
+					+ "-fx-background-color: rgba(255, 255, 255, .8);"
+					+ "-fx-border-color: #000000;"
+					+ "-fx-border-radius: 5px;"
+					+ "-fx-background-radius: 5px;"
+					+ "-fx-font-size: 15px;"
+					+ "-fx-pref-width: 300px;"
+					+ "-fx-pref-height: 200px;"
+					+ "-fx-padding: 6 6 6 6;");
+			this.getChildren().add(startPageInfo);
+			AnchorPane.setTopAnchor(startPageInfo,40.0);
+			AnchorPane.setLeftAnchor(startPageInfo, 40.0);
+			AnchorPane.setRightAnchor(startPageInfo, 40.0);
 			
 		}
 	}
-	public void refresh() {
-		if(pageNow=="Start") {
-			
+	
+	public class ManagePage extends AnchorPane{
+		class ServerView extends HBox{
+			public ServerView(Server currentServer) {
+				super(50);
+				this.setStyle("-fx-text-fill:#000000;"
+					+ "-fx-background-color: rgba(255, 255, 255, .8);"
+					+ "-fx-border-color: #000000;"
+					+ "-fx-border-radius: 5px;"
+					+ "-fx-background-radius: 5px;"
+					+ "-fx-font-size: 15px;");
+				this.setSpacing(20);
+				this.setPadding(new Insets(5, 5, 5, 5));
+				Button showCondition=new Button();
+				Label showName=new Label(currentServer.serverName);
+				showName.setMaxWidth(Double.MAX_VALUE);
+				VBox.setVgrow(showName, Priority.ALWAYS);
+				Button settingsButton=new Button();
+				this.getChildren().addAll(showCondition,showName,settingsButton);
+				this.prefWidthProperty().bind(page.widthProperty());
+			}
+		}
+		public ManagePage() {
+			super();
+			VBox serverList=new VBox();
+//			this.setStyle("-fx-background-color:#FFFFFF");//调试用-显示范围
+			this.setPadding(new Insets(5, 5, 5, 5));
+			this.getChildren().add(serverList);
+			this.prefHeightProperty().bind(page.heightProperty());
+			this.prefWidthProperty().bind(page.widthProperty());
+			AnchorPane.setTopAnchor(serverList,null);
+			AnchorPane.setLeftAnchor(serverList, null);
+			AnchorPane.setBottomAnchor(serverList, null);
+			AnchorPane.setRightAnchor(serverList, null);
+			serverList.setPadding(new Insets(0));
+			Server aServer=new Server("Server1");
+			Server bServer=new Server("Server2");
+			serverList.getChildren().add(new ServerView(aServer));
+			serverList.getChildren().add(new ServerView(bServer));
 		}
 	}
-
+	
 	public void setButtonType(Button menuButton, boolean type) {
-		menuButton.setDisable(!type);
+		menuButton.setDisable(type);
 		menuButton.setStyle("-fx-opacity: 1;");
 	}
 
@@ -66,15 +129,28 @@ public class Gui extends Application {
 //        AnchorPane.setRightAnchor(close, 0.0);
 //        AnchorPane.setTopAnchor(close, 5.0);
 //        top.getChildren().add(title);
+		
 
 		primaryStage.getIcons().add(new Image(Gui.class.getResourceAsStream("/com/mcsl/resouse/pic/ico.png")));
 		primaryStage.setTitle("MCSL");
 		primaryStage.setWidth(810);
 		primaryStage.setHeight(490);
-		BorderPane pane = new BorderPane();
+		primaryStage.setMinWidth(810);
+		primaryStage.setMinHeight(490);
+		
+		
 		pane.setId("biggest_pane");
 		pane.setPadding(new Insets(0, 0, 0, 0));// 下左上右
-		pane.setStyle("-fx-background-color: #FFFFF1；-fx-background-image: url(\"/com/mcsl/resouse/pic/background.png\");");// 背景
+		pane.setStyle("-fx-background-color:rgba(255, 255, 255, .0);");// 背景
+		
+		
+		
+//		BackgroundImage myBI= new BackgroundImage(new Image("/com/mcsl/resouse/pic/background2.png",810,490,false,true),
+//		        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+//		          BackgroundSize.DEFAULT);
+//		//then you set to your node
+//		pane.setBackground(new Background(myBI));
+//		//老的背景实现
 
 		Image start_unselected = new Image("/com/mcsl/resouse/pic/start_unselected.png");
 		Image start_selected = new Image("/com/mcsl/resouse/pic/start_selected.png");
@@ -84,6 +160,7 @@ public class Gui extends Application {
 		Image run_selected = new Image("/com/mcsl/resouse/pic/run_selected.png");
 		Image manage_unselected = new Image("/com/mcsl/resouse/pic/manage_unselected.png");
 		Image manage_selected = new Image("/com/mcsl/resouse/pic/manage_selected.png");
+		//加载菜单按钮图片资源
 		Button menuButton_Start = new Button("");
 		Button menuButton_Manage = new Button("");
 		Button menuButton_Run = new Button("");
@@ -92,112 +169,135 @@ public class Gui extends Application {
 		menuButton_Manage.setGraphic(new ImageView(manage_unselected));
 		menuButton_Run.setGraphic(new ImageView(run_unselected));
 		menuButton_Settings.setGraphic(new ImageView(settings_unselected));
-//		final String MENU_UNSELECTED = "-fx-text-fill:#1375D8;-fx-background-color: #FFFFFF;-fx-border-color: #FFFFFF;-fx-border-radius: 20;-fx-background-radius: 20;-fx-pref-width: 100px;-fx-pref-height: 35px;-fx-padding: 6 6 6 6;";
-//		final String MENU_SELECTED = "-fx-text-fill:#FFFFFF;-fx-background-color: #1375D8;-fx-border-color: #1375D8;-fx-border-radius: 20;-fx-background-radius: 20;-fx-pref-width: 100px;-fx-pref-height: 35px;-fx-padding: 6 6 6 6;";
-//		menuButton_Start.setStyle(MENU_SELECTED);
-//		menuButton_Manage.setStyle(MENU_UNSELECTED);
-//		menuButton_Run.setStyle(MENU_UNSELECTED);
-//		menuButton_Settings.setStyle(MENU_UNSELECTED);
-
-		// 加载默认样式
+		//设置按钮
 		menuButton_Start.setId("menu");
 		menuButton_Manage.setId("menu");
 		menuButton_Run.setId("menu");
 		menuButton_Settings.setId("menu");
-
-		// 设置default page
+		// 加载默认样式
+		
+		page.setStyle("-fx-background-color:rgba(255, 255, 255, .0);");
+		//设置背景透明
+		
 		pageNow = "Start";
-		setButtonType(menuButton_Start, false);
+		setButtonType(menuButton_Start, true);
 		menuButton_Start.setGraphic(new ImageView(start_selected));
-		setButtonType(menuButton_Manage, true);
+		setButtonType(menuButton_Manage, false);
 		menuButton_Manage.setGraphic(new ImageView(manage_unselected));
-		setButtonType(menuButton_Run, true);
+		setButtonType(menuButton_Run, false);
 		menuButton_Run.setGraphic(new ImageView(run_unselected));
-		setButtonType(menuButton_Settings, true);
+		setButtonType(menuButton_Settings, false);
 		menuButton_Settings.setGraphic(new ImageView(settings_unselected));
+		page.getChildren().clear();
+		page.getChildren().add(new StartPage());
 
 		menuButton_Start.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Start, false);
+				setButtonType(menuButton_Start, true);
 				menuButton_Start.setGraphic(new ImageView(start_selected));
-				setButtonType(menuButton_Manage, true);
+				setButtonType(menuButton_Manage, false);
 				menuButton_Manage.setGraphic(new ImageView(manage_unselected));
-				setButtonType(menuButton_Run, true);
+				setButtonType(menuButton_Run, false);
 				menuButton_Run.setGraphic(new ImageView(run_unselected));
-				setButtonType(menuButton_Settings, true);
+				setButtonType(menuButton_Settings, false);
 				menuButton_Settings.setGraphic(new ImageView(settings_unselected));
 				pageNow = "Start";
-				refresh();
+				page.getChildren().clear();
+				page.getChildren().add(new StartPage());
+				
 			}
 		});
 		menuButton_Manage.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Manage, false);
+				setButtonType(menuButton_Manage, true);
 				menuButton_Manage.setGraphic(new ImageView(manage_selected));
-				setButtonType(menuButton_Start, true);
+				setButtonType(menuButton_Start, false);
 				menuButton_Start.setGraphic(new ImageView(start_unselected));
-				setButtonType(menuButton_Run, true);
+				setButtonType(menuButton_Run, false);
 				menuButton_Run.setGraphic(new ImageView(run_unselected));
-				setButtonType(menuButton_Settings, true);
+				setButtonType(menuButton_Settings, false);
 				menuButton_Settings.setGraphic(new ImageView(settings_unselected));
 				pageNow = "Manage";
-				refresh();
+				page.getChildren().clear();
+				page.getChildren().add(new ManagePage());
 			}
 		});
 		menuButton_Run.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Run, false);
+				setButtonType(menuButton_Run, true);
 				menuButton_Run.setGraphic(new ImageView(run_selected));
-				setButtonType(menuButton_Start, true);
+				setButtonType(menuButton_Start, false);
 				menuButton_Start.setGraphic(new ImageView(start_unselected));
-				setButtonType(menuButton_Manage, true);
+				setButtonType(menuButton_Manage, false);
 				menuButton_Manage.setGraphic(new ImageView(manage_unselected));
-				setButtonType(menuButton_Settings, true);
+				setButtonType(menuButton_Settings, false);
 				menuButton_Settings.setGraphic(new ImageView(settings_unselected));
 				pageNow = "Run";
-				refresh();
+				page.getChildren().clear();
 			}
 		});
 		menuButton_Settings.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				setButtonType(menuButton_Settings, false);
+				setButtonType(menuButton_Settings, true);
 				menuButton_Settings.setGraphic(new ImageView(settings_selected));
-				setButtonType(menuButton_Start, true);
+				setButtonType(menuButton_Start, false);
 				menuButton_Start.setGraphic(new ImageView(start_unselected));
-				setButtonType(menuButton_Manage, true);
+				setButtonType(menuButton_Manage, false);
 				menuButton_Manage.setGraphic(new ImageView(manage_unselected));
-				setButtonType(menuButton_Run, true);
+				setButtonType(menuButton_Run, false);
 				menuButton_Run.setGraphic(new ImageView(run_unselected));
 				pageNow = "Settings";
-				refresh();
+				page.getChildren().clear();
 			}
 		});
 
-		HBox menuBox = new HBox(30);
+		HBox menuBox = new HBox(40);
 		menuBox.getChildren().addAll(menuButton_Start, menuButton_Manage, menuButton_Run, menuButton_Settings);
 		menuBox.setAlignment(Pos.CENTER);
-		menuBox.setPrefHeight(50);
-		menuBox.setPadding(new Insets(8));
+//		menuBox.setPrefHeight(30);
+		menuBox.setPadding(new Insets(0));
 		menuButton_Start.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Manage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Run.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Settings.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-		menuBox.setStyle("-fx-background-color: #FFFFFF");
+		menuBox.setStyle("-fx-background-color: #FFFFFF;-fx-opacity: 0.6;");
 		HBox.setHgrow(menuButton_Start, Priority.ALWAYS);
 		HBox.setHgrow(menuButton_Manage, Priority.ALWAYS);
 		HBox.setHgrow(menuButton_Run, Priority.ALWAYS);
 		HBox.setHgrow(menuButton_Settings, Priority.ALWAYS);
 		
 
+
+		AnchorPane backgroundPane=new AnchorPane();
+		Label backgroundLabel=new Label();
+		ImageView backgroundImageView=new ImageView(new Image(getClass().getResourceAsStream("/com/mcsl/resouse/pic/background1.png")));
+
+		
+		backgroundImageView.fitHeightProperty().bind(pane.widthProperty());
+		backgroundImageView.fitWidthProperty().bind(pane.widthProperty());
+		backgroundLabel.setGraphic(backgroundImageView);
+		
+		backgroundPane.getChildren().add(backgroundLabel);
+		AnchorPane.setTopAnchor(backgroundLabel, (double) 0);
+		AnchorPane.setLeftAnchor(backgroundLabel, (double) 0);
+		AnchorPane.setRightAnchor(backgroundLabel, (double) 0);
+		AnchorPane.setBottomAnchor(backgroundLabel, (double) 0);
+		backgroundPane.getChildren().add(pane);
+		AnchorPane.setTopAnchor(pane, (double) 0);
+		AnchorPane.setLeftAnchor(pane, (double) 0);
+		AnchorPane.setRightAnchor(pane, (double) 0);
+		AnchorPane.setBottomAnchor(pane, (double) 0);
+		
 		pane.setBottom(menuBox);
+		pane.setCenter(page);
 		pane.getStylesheets().add(Gui.class.getResource("/com/mcsl/resouse/css/style.css").toString());
 //		root.getChildren().addAll(top,pane);
 
-		Scene scene = new Scene(pane, 200, 250);
+		Scene scene = new Scene(backgroundPane);
 //		primaryStage.initStyle(StageStyle.TRANSPARENT);
 		primaryStage.setScene(scene);
 		primaryStage.show();
