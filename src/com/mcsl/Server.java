41eb04b4f4ebd8f423aee4733d89bfb244e49c13
Bluemangoo <chenfy2006@qq.com>
2021-06-24 17:34:21 +0800

    2021年6月24日17点34分

--- src/com/mcsl/Server.java
+++ src/com/mcsl/Server.java
@@ -3,15 +3,29 @@ package com.mcsl;
 import java.util.*;
 import java.awt.*;
 import java.io.File;
+import java.nio.file.Path;
+
 import javax.swing.*;
 
 public class Server {
-	String serverName;
-	File fileSettings, fileOptions;
+	String serverName,serverType,serverVersion,serverCondition;
+	
+	boolean pluginForMCDR,pluginForBucket,modForForge,modForFabric;
+	
+	String javaEnvPath,pythonenvPath;
+	
+	int minMemory,maxMemory;
+	String jvmParameter;
+
 	Properties serverSettings, serverOptions;
 
 	public Server(String theName) {
 		this.serverName = theName;
+		try {
+			Util.system("cmd");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
