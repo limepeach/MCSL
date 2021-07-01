eed64a070ffd188a6602c1273f01809818975a19
Bluemangoo <chenfy2006@qq.com>
2021-07-01 09:41:40 +0800

    修改菜单栏

--- src/com/mcsl/Gui.java
+++ src/com/mcsl/Gui.java
@@ -6,6 +6,7 @@ import javafx.geometry.*;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.*;
 import javafx.event.*;
@@ -16,12 +17,27 @@ public class Gui extends Application {
 	}
 
 
+
+
+////	窗口拖拉拽用
+//	private static boolean isRight;// 是否处于右边界调整窗口状态
+//	private static boolean isLeft;// 是否处于左边界调整窗口状态
+//	private static boolean isUp;// 是否处于上边界调整窗口状态
+//	private static boolean isBottom;// 是否处于下边界调整窗口状态
+//	private static boolean isBottomRight;// 是否处于右下角调整窗口状态
+//	private final static int RESIZE_WIDTH = 5;// 判定是否为调整窗口状态的范围与边界距离
+//	private final static double MIN_WIDTH = 810;// 窗口最小宽度
+//	private final static double MIN_HEIGHT = 490;// 窗口最小高度
+
 	Pane page = new Pane();
 	BorderPane pane = new BorderPane();
 	String pageNow;
 
 
-	public class StartPage extends AnchorPane {
+
+
+
+	public static class StartPage extends AnchorPane {
 		public StartPage() {
 			super();
 			this.setStyle("-fx-background-color:rgba(255, 255, 255, .0);");
@@ -144,7 +160,7 @@ public class Gui extends Application {
 		Server.serverList.add(new Server("ServerA"));
 		Server.serverList.add(new Server("ServerA"));
 
-		primaryStage.getIcons().add(new Image(Gui.class.getResourceAsStream("/com/mcsl/resource/pic/ico.png")));
+		primaryStage.getIcons().add(new Image("/com/mcsl/resource/pic/ico.png"));
 		primaryStage.setTitle("MCSL");
 		primaryStage.setWidth(810);
 		primaryStage.setHeight(490);
@@ -204,14 +220,14 @@ public class Gui extends Application {
 
 
 
-		Image start_unselected = new Image("/com/mcsl/resource/pic/start_unselected.png");
-		Image start_selected = new Image("/com/mcsl/resource/pic/start_selected.png");
-		Image settings_unselected = new Image("/com/mcsl/resource/pic/settings_unselected.png");
-		Image settings_selected = new Image("/com/mcsl/resource/pic/settings_selected.png");
-		Image run_unselected = new Image("/com/mcsl/resource/pic/run_unselected.png");
-		Image run_selected = new Image("/com/mcsl/resource/pic/run_selected.png");
-		Image manage_unselected = new Image("/com/mcsl/resource/pic/manage_unselected.png");
-		Image manage_selected = new Image("/com/mcsl/resource/pic/manage_selected.png");
+		Image start_unselected = new Image("/com/mcsl/resource/pic/start_unselected.png", 30, 30, false, true);
+		Image start_selected = new Image("/com/mcsl/resource/pic/start_selected.png", 30, 30, false, true);
+		Image settings_unselected = new Image("/com/mcsl/resource/pic/settings_unselected.png", 30, 30, false, true);
+		Image settings_selected = new Image("/com/mcsl/resource/pic/settings_selected.png", 30, 30, false, true);
+		Image run_unselected = new Image("/com/mcsl/resource/pic/run_unselected.png", 30, 30, false, true);
+		Image run_selected = new Image("/com/mcsl/resource/pic/run_selected.png", 30, 30, false, true);
+		Image manage_unselected = new Image("/com/mcsl/resource/pic/manage_unselected.png", 30, 30, false, true);
+		Image manage_selected = new Image("/com/mcsl/resource/pic/manage_selected.png", 30, 30, false, true);
 		//加载菜单按钮图片资源
 		Button menuButton_Start = new Button("");
 		Button menuButton_Manage = new Button("");
@@ -276,7 +292,7 @@ public class Gui extends Application {
 				page.getChildren().add(new ManagePage());
 			}
 		});
-		menuButton_Run.setOnAction(new EventHandler<ActionEvent>() {
+		menuButton_Run.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle(ActionEvent event) {
 				setButtonType(menuButton_Run, true);
@@ -310,17 +326,20 @@ public class Gui extends Application {
 		HBox menuBox = new HBox(40);
 		menuBox.getChildren().addAll(menuButton_Start, menuButton_Manage, menuButton_Run, menuButton_Settings);
 		menuBox.setAlignment(Pos.CENTER);
-//		menuBox.setPrefHeight(30);
-		menuBox.setPadding(new Insets(0));
+		menuBox.setPadding(new Insets(5));
 		menuButton_Start.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Manage.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Run.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuButton_Settings.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 		menuBox.setStyle("-fx-background-color: #FFFFFF;-fx-opacity: 0.6;");
-		HBox.setHgrow(menuButton_Start, Priority.ALWAYS);
-		HBox.setHgrow(menuButton_Manage, Priority.ALWAYS);
-		HBox.setHgrow(menuButton_Run, Priority.ALWAYS);
-		HBox.setHgrow(menuButton_Settings, Priority.ALWAYS);
+//		HBox.setHgrow(menuButton_Start, Priority.ALWAYS);
+//		HBox.setHgrow(menuButton_Manage, Priority.ALWAYS);
+//		HBox.setHgrow(menuButton_Run, Priority.ALWAYS);
+//		HBox.setHgrow(menuButton_Settings, Priority.ALWAYS);
+		menuButton_Start.prefWidthProperty().bind(menuBox.widthProperty().divide(8));
+		menuButton_Manage.prefWidthProperty().bind(menuBox.widthProperty().divide(8));
+		menuButton_Run.prefWidthProperty().bind(menuBox.widthProperty().divide(8));
+		menuButton_Settings.prefWidthProperty().bind(menuBox.widthProperty().divide(8));
 
 
 		AnchorPane backgroundPane = new AnchorPane();
@@ -349,8 +368,69 @@ public class Gui extends Application {
 //		root.getChildren().addAll(top,pane);
 
 		Scene scene = new Scene(backgroundPane);
-		primaryStage.initStyle(StageStyle.TRANSPARENT);
+		primaryStage.initStyle(StageStyle.DECORATED);
 		primaryStage.setScene(scene);
+
+////		窗口拖拉拽
+//			backgroundPane.setOnMouseMoved((MouseEvent event) -> {
+//			event.consume();
+//			double x = event.getSceneX();
+//			double y = event.getSceneY();
+//			double width = primaryStage.getWidth();
+//			double height = primaryStage.getHeight();
+//			Cursor cursorType = Cursor.DEFAULT;// 鼠标光标初始为默认类型，若未进入调整窗口状态，保持默认类型
+//			// 先将所有调整窗口状态重置
+//			isRight = isLeft = isUp = isBottom = isBottomRight = false;
+//			if (y >= height - RESIZE_WIDTH) {
+//				if (x <= RESIZE_WIDTH) {// 左下角调整窗口状态
+//
+//				} else if (x >= width - RESIZE_WIDTH) {// 右下角调整窗口状态
+//					isBottomRight = true;
+//					cursorType = Cursor.SE_RESIZE;
+//				} else {// 下边界调整窗口状态
+//					isBottom = true;
+//					cursorType = Cursor.S_RESIZE;
+//				}
+//			} else if (x >= width - RESIZE_WIDTH) {// 右边界调整窗口状态
+//				isRight = true;
+//				cursorType = Cursor.E_RESIZE;
+//			}
+//			// 最后改变鼠标光标
+//			backgroundPane.setCursor(cursorType);
+//		});
+//		backgroundPane.setOnMouseDragged((MouseEvent event) -> {
+//			double x = event.getSceneX();
+//			double y = event.getSceneY();
+//			// 保存窗口改变后的x、y坐标和宽度、高度，用于预判是否会小于最小宽度、最小高度
+//			double nextX = primaryStage.getX();
+//			double nextY = primaryStage.getY();
+//			double nextWidth = primaryStage.getWidth();
+//			double nextHeight = primaryStage.getHeight();
+//			if (isRight || isBottomRight) {// 所有右边调整窗口状态
+//				nextWidth = x;
+//			}
+//			if (isRight || isBottomRight) {// 所有右边调整窗口状态
+//				nextWidth = x;
+//			}
+//			if (isBottomRight || isBottom) {// 所有下边调整窗口状态
+//				nextHeight = y;
+//			}
+//			if (isBottomRight || isBottom) {// 所有下边调整窗口状态
+//				nextHeight = y;
+//			}
+//			if (nextWidth <= MIN_WIDTH) {// 如果窗口改变后的宽度小于最小宽度，则宽度调整到最小宽度
+//				nextWidth = MIN_WIDTH;
+//			}
+//			if (nextHeight <= MIN_HEIGHT) {// 如果窗口改变后的高度小于最小高度，则高度调整到最小高度
+//				nextHeight = MIN_HEIGHT;
+//			}
+//			// 最后统一改变窗口的x、y坐标和宽度、高度，可以防止刷新频繁出现的屏闪情况
+//			primaryStage.setX(nextX);
+//			primaryStage.setY(nextY);
+//			primaryStage.setWidth(nextWidth);
+//			primaryStage.setHeight(nextHeight);
+//		});
+
 		primaryStage.show();
 
 	}
